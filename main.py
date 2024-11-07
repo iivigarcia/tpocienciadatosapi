c6cbf9702df0ca2c017edb08e9f022518353290f
ivan <43122250+iivigarcia@users.noreply.github.com>
2024-11-07 14:46:45 -0300

    add

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import Optional
-
+import pandas as pd
+import json
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +12,15 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/data")
+async def read_data():
+    new_data = pd.read_csv('content/data.csv', on_bad_lines='skip')
+    new_data.to_json("content/data.json")
+    with open('content/data.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
